7b9f9942b6cadd47fedfd0857863cd777e8a94e6
Misael <misael.marques@ufape.edu.br>
2026-08-23 21:04:18 -0300

    feat: implementa interface e rotas de api para visualizacao das views

--- Sistema/src/lib/db.js
+++ Sistema/src/lib/db.js
@@ -1,11 +1,11 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  user: 'admin_adega',       
-  password: 'admin_senha_123',  
-  host: 'localhost',      
-  port: 5433,             
-  database: 'emporio_bebidas'  
+  user: process.env.DB_USER || 'admin_adega',
+  password: process.env.DB_PASSWORD || 'admin_senha_123',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 5433,
+  database: process.env.DB_DATABASE || 'emporio_bebidas'
 });
 
 export default pool;
